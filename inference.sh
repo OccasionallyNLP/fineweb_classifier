3935d0ff9ca9c03a7ea59a14268cbdb3681ea08e
OccasionallyNLP <111713221+OccasionallyNLP@users.noreply.github.com>
2024-10-01 16:12:26 +0900

    Update inference.sh

--- inference.sh
+++ inference.sh
@@ -1,1 +1,3 @@
-accelerate launch inference.py --batch_size 1024 --test_data /home/work/g-earth-22/hoyoun/dclm_baseline_1.0_train_000 --output_dir dclm_baseline_1.0_train_000
+accelerate launch --num_processes=8 inference_latest.py --batch_size 1024 --test_data /home/work/g-earth-22/hoyoun/dclm_baseline_1.0_train_000/dclm_baseline_1.0_train_00.jsonl --output_dir dclm_baseline_1.0_train_00
+
+# accelerate launch --num_processes=8 inference.py --batch_size 1024 --test_data /home/work/g-earth-22/hoyoun/dclm_baseline_1.0_train_000_1.jsonl --output_dir dclm_baseline_1.0_train_0 
